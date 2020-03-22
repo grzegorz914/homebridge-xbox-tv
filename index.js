416cfbe2d809cbd02bfb34bb37ecced75348f861
Grzegorz <grzegorz914@icloud.com>
2020-03-22 10:05:38 +0100

    Update index.js

--- index.js
+++ index.js
@@ -7,14 +7,13 @@ var SystemInputChannel = require('xbox-smartglass-core-node/src/channels/systemi
 var SystemMediaChannel = require('xbox-smartglass-core-node/src/channels/systemmedia');
 var TvRemoteChannel = require('xbox-smartglass-core-node/src/channels/tvremote');
 
-var Accessory, Service, Characteristic, hap, UUIDGen;
+var Accessory, Service, Characteristic, UUIDGen;
 
-module.exports = homebridge => {
+module.exports = function(homebridge) {
 	Service = homebridge.hap.Service;
 	Characteristic = homebridge.hap.Characteristic;
 	Accessory = homebridge.platformAccessory;
 	UUIDGen = homebridge.hap.uuid;
-	hap = homebridge.hap;
 
 	homebridge.registerPlatform('homebridge-xbox-tv', 'XboxTv', xboxTvPlatform, true);
 };
