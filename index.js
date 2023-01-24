af7c0f00fc8bb81a41a230084c902462e1266884
Grzegorz <grzegorz914@icloud.com>
2023-01-24 19:45:03 +0100

    fix publish accessory

--- index.js
+++ index.js
@@ -1035,22 +1035,20 @@ class XBOXDEVICE {
 				//get sensor display type
 				const inputSensorDisplayType = inputSensor.displayType || -1;
 
-				if (inputSensorDisplayType === -1) {
-					return;
+				if (inputSensorDisplayType >= 0) {
+					const serviceType = [Service.MotionSensor, Service.OccupancySensor, Service.ContactSensor][inputSensorDisplayType];
+					const characteristicType = [Characteristic.MotionDetected, Characteristic.OccupancyDetected, Characteristic.ContactSensorState][inputSensorDisplayType];
+					const inputSensorService = new serviceType(`${accessoryName} ${inputSensorName}`, `Sensor ${inputSensorName}`);
+					inputSensorService.getCharacteristic(characteristicType)
+						.onGet(async () => {
+							const state = this.power ? (this.reference === inputSensorReference) : false;
+							return state;
+						});
+
+					this.inputsSensorsReference.push(inputSensorReference);
+					this.inputSensorServices.push(inputSensorService);
+					accessory.addService(this.inputSensorServices[i]);
 				}
-
-				const serviceType = [Service.MotionSensor, Service.OccupancySensor, Service.ContactSensor][inputSensorDisplayType];
-				const characteristicType = [Characteristic.MotionDetected, Characteristic.OccupancyDetected, Characteristic.ContactSensorState][inputSensorDisplayType];
-				const inputSensorService = new serviceType(`${accessoryName} ${inputSensorName}`, `Sensor ${inputSensorName}`);
-				inputSensorService.getCharacteristic(characteristicType)
-					.onGet(async () => {
-						const state = this.power ? (this.reference === inputSensorReference) : false;
-						return state;
-					});
-
-				this.inputsSensorsReference.push(inputSensorReference);
-				this.inputSensorServices.push(inputSensorService);
-				accessory.addService(this.inputSensorServices[i]);
 			}
 		}
 
