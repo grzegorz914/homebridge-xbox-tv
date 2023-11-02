07793d70325fd12eab3c3ae6565813956c394e1b
Grzegorz <grzegorz914@icloud.com>
2023-11-02 10:02:16 +0100

    add check exist configured inputs name and target visibility

--- src/xboxdevice.js
+++ src/xboxdevice.js
@@ -824,6 +824,11 @@ class XboxDevice extends EventEmitter {
                                 return inputName;
                             })
                             .onSet(async (value) => {
+                                const valueExist = value === this.savedInputsNames[inputReference];
+                                if (valueExist) {
+                                    return;
+                                }
+
                                 try {
                                     this.savedInputsNames[inputReference] = value;
                                     const newCustomName = JSON.stringify(this.savedInputsNames, null, 2);
@@ -843,6 +848,11 @@ class XboxDevice extends EventEmitter {
                                 return targetVisibility;
                             })
                             .onSet(async (state) => {
+                                const stateExist = state === this.savedInputsTargetVisibility[inputReference];
+                                if (stateExist) {
+                                    return;
+                                };
+
                                 try {
                                     this.savedInputsTargetVisibility[inputReference] = state;
                                     const newTargetVisibility = JSON.stringify(this.savedInputsTargetVisibility, null, 2);
