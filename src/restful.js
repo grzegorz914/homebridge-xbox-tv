61d84bcf208fc596befd92f26aff5db0f0a128b3
Grzegorz <grzegorz914@icloud.com>
2024-04-15 15:44:27 +0200

    Update restful.js

--- src/restful.js
+++ src/restful.js
@@ -65,6 +65,7 @@ class RestFul extends EventEmitter {
                 this.restFulData.status = data;
                 break;
             default:
+                this.emit('debug', `RESTFul update unknown path: ${path}, data: ${data}`)
                 break;
         };
         const emitDebug = this.restFulDebug ? this.emit('debug', `RESTFul update path: ${path}, data: ${JSON.stringify(data, null, 2)}`) : false;
